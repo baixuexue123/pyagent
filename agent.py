3d3499beb39a79ad81d8306ce1dac10fff65a8fd
bxf_100_u <baixuexue123@hotmail.com>
2016-11-22 10:39:15 +0800

    system info

--- agent.py
+++ agent.py
@@ -25,13 +25,39 @@ from SimpleXMLRPCServer import SimpleXMLRPCServer
 import psutil
 import netifaces
 
-
 __version__ = '0.0.1'
 
 
 # **********************************************************************************
 # 获取机器的静态信息
 # **********************************************************************************
+
+def _linux_os_release():
+    """Try to determine the name of a Linux distribution.
+    This function checks for the /etc/os-release file.
+    It takes the name from the 'NAME' field and the version from 'VERSION_ID'.
+    An empty string is returned if the above values cannot be determined.
+    """
+    pretty_name = ''
+    ashtray = {}
+    keys = ['NAME', 'VERSION_ID']
+    try:
+        with open(os.path.join('/etc', 'os-release')) as f:
+            for line in f:
+                for key in keys:
+                    if line.startswith(key):
+                        ashtray[key] = line.strip().split('=')[1][1:-1]
+    except (OSError, IOError):
+        return pretty_name
+
+    if ashtray:
+        if 'NAME' in ashtray:
+            pretty_name = ashtray['NAME']
+        if 'VERSION_ID' in ashtray:
+            pretty_name += ' {}'.format(ashtray['VERSION_ID'])
+
+    return pretty_name
+
 
 VENDOR_PATTERN = re.compile(r'vendor_id([ \t])+: (?P<vendor_id>.*)\n')
 MODEL_PATTERN = re.compile(r'model name([ \t])+: (?P<model_name>.*)\n')
@@ -391,6 +417,50 @@ class NodeService(object):
     def __init__(self, node):
         self.node = node
 
+    def is_alive(self):
+        return '*'
+
+    def get_version(self):
+        return __version__
+
+    def get_psutil_version(self):
+        return psutil.__version__
+
+    def get_system(self):
+        _system = {
+            'hostname': platform.node(),
+            'os_name': platform.system(),
+            'os_verbose': platform.platform(),
+            'platform': platform.architecture()[0],
+            'os_version': platform.release(),
+        }
+        linux_distro = platform.linux_distribution()
+        if linux_distro[0] == '':
+            _system['linux_distro'] = _linux_os_release()
+        else:
+            _system['linux_distro'] = ' '.join(linux_distro[:2])
+        return _system
+
+    def quicklook(self):
+        return
+
+    def get_netcard_info(self):
+        return netcard_info()
+
+    def get_runtime_info(self):
+        return {
+            'hostname': socket.gethostname(),
+            'users': users(),
+            'cpu': cpu(),
+            'memory': memory(),
+            'swap': swap_memory(),
+            'disks': disks(),
+            'uptime': datetime.datetime.fromtimestamp(psutil.boot_time()).strftime('%Y-%m-%d %H:%M:%S'),
+        }
+
+    def get_processes_info(self):
+        return process_list()
+
     def add_task(self, task_info):
         logger.info('Received task: %s' % task_info)
         try:
@@ -431,29 +501,6 @@ class NodeService(object):
                 except psutil.AccessDenied:
                     return False
         return False
-
-    def is_alive(self):
-        return True
-
-    def get_base_info(self):
-        return base_info()
-
-    def get_netcard_info(self):
-        return netcard_info()
-
-    def get_runtime_info(self):
-        return {
-            'hostname': socket.gethostname(),
-            'users': users(),
-            'cpu': cpu(),
-            'memory': memory(),
-            'swap': swap_memory(),
-            'disks': disks(),
-            'uptime': datetime.datetime.fromtimestamp(psutil.boot_time()).strftime('%Y-%m-%d %H:%M:%S'),
-        }
-
-    def get_processes_info(self):
-        return process_list()
 
 
 class MarmotAgent(object):
